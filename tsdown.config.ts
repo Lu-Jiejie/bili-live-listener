6f12ed5b7123d48a7f1df5306ff5bafce9f06dda
Lu-Jiejie <lu-jiejie@outlook.com>
2026-01-15 12:36:02 +0800

    chore: tsdown and workflows

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: [
-    './src/index.ts',
+    'src/index.ts',
   ],
-  clean: true,
-  format: ['esm', 'cjs'],
-  platform: 'neutral',
+  dts: true,
+  exports: true,
+  publint: true,
 })
